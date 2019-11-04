82720653d9958c7fbb9ec1736155ff0a3dc5edcd
CesarAntolinez <cesar.antolinez@ideco.com.co>
2019-11-04 17:43:01 -0500

    agregar el esquema de autenticacion

--- routes/web.php
+++ routes/web.php
@@ -11,42 +11,6 @@
 |
 */
 
-/*
- * Get, Post, Put, Delete
- *//*
-Route::get('/', function () {
-    return view('admin.template.body');
-});
-
-Route::get('article', function (){
-    return "Esta es la seccion de articulos";
-});*/
-/*
- * Parametros
- */
-/*Route::get('articles/{nombre?}', function ($nombre = "Sin nombre"){
-    return "Este es el nombre " . $nombre;
-});*/
-/*
- * Nombres a las rutas
- */
-/*Route::get('articles', [
-    'as' => 'articles', // nombre
-    'uses' => 'UserController@index' // Controlador
-]);*/
-
-/*
- * Gurpos de rutas
- *//*
-Route::group([ 'prefix' => 'articles'], function (){
-    Route::get('view2/{article?}', function ($article = "vacio"){
-        return "Este es el articulo : " . $article;
-    });
-    //Controlador
-    Route::get('view/{id}', [
-        'uses' => 'TestController@view'
-    ]);
-});*/
 Route::get('/', function () {
     return view('admin.template.body');
 });
@@ -66,3 +30,7 @@ Route::group([ 'as'=>'admin.', 'prefix' => 'admin'], function (){
         'as'    => 'categories.destroy'
     ]);
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
