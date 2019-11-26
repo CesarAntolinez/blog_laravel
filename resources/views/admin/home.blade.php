5f64928a52e51b8409686ddd66591559e62ccce6
CesarAntolinez <cesar.antolinez@ideco.com.co>
2019-11-25 21:25:40 -0500

    mjorar el inicio

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.template.main')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Inicio</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item active">Inicio</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="content">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Inicio</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fas fa-minus"></i></button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    @include('admin.template.message')
+
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
