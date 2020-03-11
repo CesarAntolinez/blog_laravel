c2774599693738b9f20d9691bbe88eda9557d595
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2020-03-10 19:49:47 -0500

    agregar dos usarios por seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'      => 'admin 1',
+            'email'     => 'admin1@gmail.com',
+            'password'  => bcrypt('password'),
+            'type'      => 'admin'
+        ]);
+        DB::table('users')->insert([
+            'name'      => 'admin 2',
+            'email'     => 'admin2@gmail.com',
+            'password'  => bcrypt('password'),
+            'type'      => 'admin'
+        ]);
+    }
+}
