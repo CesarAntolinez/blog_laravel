046ca8e0cd94bf6f46f727140b432334c68a6c97
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2020-04-11 19:30:05 -0500

    vista articulo mejorada

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -3,13 +3,73 @@
 @section('title', $article->title)
 
 @section('content')
-    <div class="row my-4">
+    <div class="row">
+        <div class="col-12">
+            <!-- Title -->
+            <h1 class="mt-4">{{ $article->title }}</h1>
 
-        <img src="{{ asset('img/articles') . '/' . $article->images->first()->name }}" alt="{{ $article->images->first()->name }}" class="img-fluid">
-        <h1>{{ $article->title }}</h1>
-    </div>
-    <div class="row my-4">
-        {!! $article->content !!}
+            <!-- Author -->
+            <p class="lead">por <span class="text-bold">{{ $article->user->name }}</span></p>
+            <hr>
+            <!-- Date/Time -->
+            <p>Creado el  {{ $article->create_at }}</p>
+            <hr>
+            <img src="{{ asset('img/articles') . '/' . $article->images->first()->name }}" alt="{{ $article->images->first()->name }}" class="img-fluid">
+        </div>
+        <!-- Preview Image -->
+        <div class="col-12">
+            {!! $article->content !!}
+        </div>
+        <div class="col-12">
+            <!-- Comments Form -->
+            <div class="card my-4">
+                <h5 class="card-header">Leave a Comment:</h5>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Single Comment -->
+            <div class="media mb-4">
+                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                <div class="media-body">
+                    <h5 class="mt-0">Commenter Name</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+            </div>
+
+            <!-- Comment with nested comments -->
+            <div class="media mb-4">
+                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                <div class="media-body">
+                    <h5 class="mt-0">Commenter Name</h5>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+                    <div class="media mt-4">
+                        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                        <div class="media-body">
+                            <h5 class="mt-0">Commenter Name</h5>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+
+                    <div class="media mt-4">
+                        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                        <div class="media-body">
+                            <h5 class="mt-0">Commenter Name</h5>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
 @endsection
